f5a95909e05de1548d6764db2dd932cd406f98b9
grewald <you@example.codeepakgrewal@gmail.com>
2021-12-29 18:26:44 +0000

    added other plots

--- plot1.R
+++ plot1.R
@@ -25,9 +25,54 @@ allElectricUsage <- allElectricUsage [2:nrow,]
 # convert  Global_active_power to numeric
 s2 <- s2 %>%  mutate(Global_active_power_n = as.numeric(Global_active_power))
 
-# generate plot
+# generate plot1
 s2 %>%  ggplot(aes(Global_active_power_n)) + geom_histogram(fill='red',bins = 30) + 
   labs(x= "Global Active Power (Killowats)", y= "Frequency")
-   
 
+# generate plot2   
+s2 %>%  ggplot(aes(x= datetimeR, y= Global_active_power_n)) + geom_line() + 
+  labs(y= "Global Active Power (Killowats)", x= "Datetime")
+
+wdayVector <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+          "Friday", "Saturday")
+
+s3 <- s2 %>%  mutate(wday= wdayVector[ as.POSIXlt(dateR)$wday +1])
+
+s3 %>%  ggplot(aes(x= datetimeR, y= Global_active_power_n)) + geom_line() + 
+  labs(y= "Global Active Power (Killowats)", x= "Datetime")
+
+ 
+s3 %>%  ggplot(aes(x= datetimeR, y= Global_active_power_n)) + geom_line() + 
+  labs(y= "Global Active Power (Killowats)", x= "Datetime" ) +
+  scale_fill_manual(values = c('Thu', 'Fri', 'Sat')) 
+
+s3 %>%  ggplot(aes(x= datetimeR, y= Global_active_power_n )) + geom_line() + 
+  labs(y= "Global Active Power (Killowats)", x= "Datetime" ) +
+  scale_fill_discrete(labels = c('Thu', 'Fri', 'Sat'))
+  
+#generate plot 3
+
+  toPlot <- s2 %>% select(starts_with('Sub_metering'), datetimeR) %>% 
+    pivot_longer(cols = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
+  
+  # toPlot <- toPlot %>% mutate(dt_n=as.numeric(toPlot$datetimeR) ,
+  #                             value_n=as.numeric(toPlot$value) )
+  # 
+  # .8 * max(toPlot$dt_n)
+  # .8 * max(toPlot$value_n)
+  # posLabel <- c(.8 * max(toPlot$dt_n), .8 * max(toPlot$value_n))
+  # 
+  # toPlot %>%  ggplot(aes(x= datetimeR,y=as.numeric(value), color= name)) + geom_line()+
+  #   labs(x= "Datetime", y= "Energy Sub Metering" ) +
+  #   theme(legend.title = element_blank() )
+  
+  toPlot %>%  ggplot(aes(x= datetimeR,y=as.numeric(value), color= name)) + geom_line()+
+  labs(x= "Datetime", y= "Energy Sub Metering" ) +
+    theme(legend.title = element_blank(), legend.position = c(.9,.9) )
+  
+  #generate plot 4
+  
+  #create the new plots first
+  
+  
                
